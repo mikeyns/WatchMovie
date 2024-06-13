c8cebe6ca14efeb4ed1ec56c3f3cebc693e1e3f9
Aluno <Aluno@10.85.198.42>
2024-06-13 14:55:15 -0300

    java project WatchMovie

---
+++ src/WatchMovie.java
@@ -0,0 +1,12 @@
+public class WatchMovie {
+    public static void main(String[] args) {
+        double precoIngresso = 12.0; // Preço do ingresso
+        int classificacaoFilme = 5; // Classificação do filme
+
+        if (precoIngresso >= 12.0 && classificacaoFilme == 5) {
+            System.out.println("Estou interessado em assistir o filme.");
+        } else {
+            System.out.println("Não estou interessado em assistir o filme.");
+        }
+    }
+}
